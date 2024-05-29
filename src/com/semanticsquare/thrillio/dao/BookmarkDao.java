2195aad23fcd58ed58c345efe46b7f1cb28e796a
Sudip Bose <sbose007ime.work@gmail.com>
2024-05-30 00:13:29 +0530

    removed in-memory database with hibernate ORM support

--- src/com/semanticsquare/thrillio/dao/BookmarkDao.java
+++ src/com/semanticsquare/thrillio/dao/BookmarkDao.java
@@ -3,21 +3,18 @@ package com.semanticsquare.thrillio.dao;
 
 import java.util.ArrayList;
 
-import com.semanticsquare.thrillio.Database;
 import com.semanticsquare.thrillio.entities.BookMark;
 import com.semanticsquare.thrillio.entities.User;
 import com.semanticsquare.thrillio.entities.UserBookmark;
 
 public class BookmarkDao {
 	
-	private Database postgresqlDatabase = new Database();
 	
 	public void addUserBookmarkToDB(UserBookmark userBookmark) {
-		postgresqlDatabase.setUserBookmark(userBookmark);
 	}
 
 	public ArrayList<BookMark> getUserBookMarks(User user) {
-		return postgresqlDatabase.getUserBookmarks(user);
+		return new ArrayList<BookMark>();
 	}
 	
 	
