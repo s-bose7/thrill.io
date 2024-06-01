31431894e07ba00118edb42290aa6bc75d44c4ca
Sudip Bose <sbose007ime.work@gmail.com>
2024-06-01 22:43:58 +0530

    Added create and read operations for User instance

--- src/com/semanticsquare/thrillio/dao/UserDao.java
+++ src/com/semanticsquare/thrillio/dao/UserDao.java
@@ -3,20 +3,65 @@ package com.semanticsquare.thrillio.dao;
 import java.util.ArrayList;
 
 import com.semanticsquare.thrillio.entities.User;
+import com.semanticsquare.thrillio.utils.HibernateUtils;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class UserDao {
 	
-	
+	// Retrieves all users from the database
 	public ArrayList<User> getAlluser() {
 		return new ArrayList<User>();
 	}
 	
-	public User checkWhetherUserExist(String email, String password) {
-		User user = null;
-		return user;
+	// Checks if a user exists in the database with the provided email and password
+	public User findUserByEmailAndPassword(String email, String password) {
+		Session session = null;
+		try {
+			session = HibernateUtils.getSessionFactory().openSession();
+            return session.createQuery("FROM User WHERE email = :email AND password = :password", User.class)
+                          .setParameter("email", email)
+                          .setParameter("password", password)
+                          .uniqueResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
 	}
 	
-	public void addUserToDB(User user) {
+	// Adds a new user to the database
+	public void insertUser(User user) {
+		Transaction transaction = null;
+		Session session = null;
+        try {
+        	session = HibernateUtils.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
 	}
 	
+	// Retrieves a user by their ID
+    public User findUserById(int userId) {
+        User user = null;
+        return user;
+    }
+    
+    // Checks if a user exists in the database by their email
+    public boolean findUserByEmail(String email) {
+        return false;
+    }
 }
